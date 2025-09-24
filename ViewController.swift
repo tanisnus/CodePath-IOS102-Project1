e9cac6b45a30126eebb822ef49033fa0869f02b1
tanisnus <taniss246@gmail.com>
2025-09-23 23:49:55 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodePath - Project 1 - Scavenger Hunt
+//
+//  Created by Tanis Sarbatananda on 9/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
